102102c2a1920863b602fb425d66f57708a803ab
abdul.aala <abdul.aala@tenpearls.com>
2018-09-28 18:13:31 +0500

    Added json config files.

---
+++ Meterzy.Api/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Meterzy.Api.Helpers;
+using Meterzy.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Meterzy.Api
+{
+    public class Startup
+    {
+        #region Variable(s)
+        private static ILoggerFactory loggerFactory;
+        #endregion
+
+        #region Constructor(s)
+        public Startup(ILoggerFactory loggerFactory)
+        {
+            Startup.loggerFactory = loggerFactory;
+        }
+        #endregion
+
+        #region Sync Method(s)
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<MeterzyContext>((options) => { options.UseSqlServer(Config.Secrets.ConnectionString); }, ServiceLifetime.Scoped, ServiceLifetime.Scoped);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+        #endregion
+
+    }
+}
