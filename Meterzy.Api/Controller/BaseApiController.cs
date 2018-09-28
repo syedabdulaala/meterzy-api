3f25dc66d7674a4533bf007b760f8291576e86d3
abdul.aala <abdul.aala@tenpearls.com>
2018-09-28 18:15:17 +0500

    Added empty base api and auth controllers

---
+++ Meterzy.Api/Controller/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meterzy.Api.Controller
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        #region Variable(s)
+        protected readonly ILogger logger;
+        #endregion
+
+        #region Constructor(s)
+        public BaseApiController(ILogger<BaseApiController> logger)
+        {
+            this.logger = logger;
+        }
+        #endregion
+    }
+}
