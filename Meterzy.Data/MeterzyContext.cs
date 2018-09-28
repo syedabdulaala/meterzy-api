3b551084224b3fefcdbabc673089a9653dd0388b
abdul.aala <abdul.aala@tenpearls.com>
2018-09-28 18:12:30 +0500

    Added database context and generic repository

---
+++ Meterzy.Data/MeterzyContext.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Meterzy.Data.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meterzy.Data
+{
+    public class MeterzyContext : DbContext
+    {
+        public MeterzyContext(DbContextOptions options) : base(options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new AppUserConfig(modelBuilder);
+            new MeterConfig(modelBuilder);
+            new TariffConfig(modelBuilder);
+            new FixedTariffConfig(modelBuilder);
+            new RangedTariffConfig(modelBuilder);
+            new MeterReadingConfig(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Meterzy.Data/Repo.cs
@@ -0,0 +1,139 @@
+﻿using Meterzy.Entity.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meterzy.Data
+{
+    public interface IRepo<T> where T : Table
+    {
+        #region Property(ies)
+        IQueryable<T> DataSet { get; }
+        #endregion
+
+        #region Async Method(s)
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetPaginatedAsync(int start, int length);
+        Task AddAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task SoftDeleteAsync(T obj);
+        Task DeleteAsync(T obj);
+        Task<int> SaveAsync();
+        #endregion
+
+        #region Sync Method(s)
+        List<T> GetAll();
+        List<T> GetPaginated(int start, int length);
+        void Add(T obj);
+        void Update(T obj);
+        void SoftDelete(T obj);
+        void Delete(T obj);
+        int Save();
+        #endregion
+    }
+    public sealed class Repo<T> : IRepo<T> where T : Table
+    {
+        #region Variable(s)
+        private readonly DbContext dbContext;
+        #endregion
+
+        #region Property(ies)
+        public IQueryable<T> DataSet => dbContext.Set<T>();
+        #endregion
+
+        #region Constructor(s)
+        public Repo(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        #endregion
+
+        #region Async Method(s)
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<List<T>> GetPaginatedAsync(int start, int length)
+        {
+            return await dbContext.Set<T>()
+                                  .Take(length)
+                                  .Skip(0).ToListAsync();
+        }
+
+        public async Task AddAsync(T obj)
+        {
+            obj.CreatedOn = DateTime.UtcNow;
+            await dbContext.AddAsync(obj);
+        }
+
+        public async Task UpdateAsync(T obj)
+        {
+            obj.LastModifiedOn = DateTime.UtcNow;
+            await Task.Run(() => { dbContext.Update(obj); });
+        }
+
+        public async Task SoftDeleteAsync(T obj)
+        {
+            obj.Deleted = true;
+            obj.LastModifiedOn = DateTime.UtcNow;
+            await UpdateAsync(obj);
+        }
+
+        public async Task DeleteAsync(T obj)
+        {
+            await Task.Run(() => { dbContext.Remove(obj); });
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Sync Method(s)
+        public List<T> GetAll()
+        {
+            return dbContext.Set<T>().ToList();
+        }
+
+        public List<T> GetPaginated(int start, int length)
+        {
+            return dbContext.Set<T>()
+                            .Take(length)
+                            .Skip(0).ToList();
+        }
+
+        public void Add(T obj)
+        {
+            obj.CreatedOn = DateTime.UtcNow;
+            dbContext.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            obj.LastModifiedOn = DateTime.UtcNow;
+            dbContext.Update(obj);
+        }
+
+        public void SoftDelete(T obj)
+        {
+            obj.Deleted = true;
+            obj.LastModifiedOn = DateTime.UtcNow;
+            Update(obj);
+        }
+
+        public void Delete(T obj)
+        {
+            dbContext.Remove(obj);
+        }
+
+        public int Save()
+        {
+            return dbContext.SaveChanges();
+        }        
+        #endregion
+    }
+}
